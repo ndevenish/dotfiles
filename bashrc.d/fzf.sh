d0d6667e461c3bc5ab1da8e9b7fe09f922ddb673
Nicholas Devenish <ndevenish@gmail.com>
2021-08-25 12:19:00 +0100

    Add fzf

---
+++ bashrc.d/fzf.sh
@@ -0,0 +1,29 @@
+
+if ! command -v fzf >/dev/null 2>&1; then
+    echo "No fzf"
+    return
+fi
+
+bind '"\C-r": "\C-x1\e^\er"'
+bind -x '"\C-x1": __fzf_history';
+
+__fzf_history ()
+{
+__ehc $(cat ~/.fullhistory | fzf --tac --with-nth 3.. -n 4.. --tiebreak=index | perl -ne 'm/^\s*([0-9]+)/ and print "!$1"')
+}
+
+__ehc()
+{
+if
+        [[ -n $1 ]]
+then
+        bind '"\er": redraw-current-line'
+        bind '"\e^": magic-space'
+        READLINE_LINE=${READLINE_LINE:+${READLINE_LINE:0:READLINE_POINT}}${1}${READLINE_LINE:+${READLINE_LINE:READLINE_POINT}}
+        READLINE_POINT=$(( READLINE_POINT + ${#1} ))
+else
+        bind '"\er":'
+        bind '"\e^":'
+fi
+}
+
