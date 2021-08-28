70aa81c451c618c6ab33aa4d15c8656458ddb625
Nicholas Devenish <ndevenish@gmail.com>
2021-08-28 18:46:08 +0100

    Use tac if available

--- bashrc.d/fzf.sh
+++ bashrc.d/fzf.sh
@@ -3,13 +3,20 @@
 if ! command -v fzf >/dev/null 2>&1; then
     return
 fi
+if ! command -v fzf >/dev/null 2>&1; then
+    return
+fi
 
 bind '"\C-r": "\C-x1\e^\er"'
 bind -x '"\C-x1": __fzf_history';
 
 __fzf_history ()
 {
-    __ehc "$(tail -r ~/.fullhistory \
+    local reverser=tac
+    if ! command -v tac >/dev/null 2>&1; then
+        reverser='tail -r'
+    fi
+    __ehc "$($reverser ~/.fullhistory \
         | fzf --with-nth 3.. -n 2.. --tiebreak=index --preview 'echo {} | cut -d" " -f 4-' --preview-window=up,3,wrap --bind change:top \
         | cut -d" " -f 4- \
         | sed 's/[ \t]*$//')"
