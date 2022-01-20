1435428601631df135126b456bba349cd81edb97
Nicholas Devenish <ndevenish@gmail.com>
2022-01-20 09:14:07 +0000

    Add coloured git prompt

--- bashrc.d/prompt.sh
+++ bashrc.d/prompt.sh
@@ -3,12 +3,14 @@
 
 export GIT_PS1_SHOWDIRTYSTATE="${GIT_PS1_SHOWDIRTYSTATE:-1}"
 
-# Insert git_prompt
+# Insert git_prompt if not present
 if  [[ ! "$PS1" =~ __git_ps1 ]]; then
     if [[ -z "$(type -t __git_ps1)" ]]; then
+        # shellcheck disable=SC1090
         source ~/.git-prompt.sh
     fi
-    PS1="$(printf "%s" "$PS1" | sed 's;\\\$;\$\(__git_ps1)\\\$;')"
+    PS1="$(printf "%s" "$PS1" | sed 's|\\\$|\\[\\e[1;34m\\]\$\(__git_ps1)\\[\\e[0m\\]\\\$|')"
+    export PS1
 fi
 
 # Add a magic "red for failure" prompt state if not there already
