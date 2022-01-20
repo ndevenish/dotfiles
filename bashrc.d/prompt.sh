ab908d17489a8bc50ed5bb804419836ca39070aa
Nicholas Devenish <ndevenish@gmail.com>
2022-01-20 08:51:58 +0000

    Add prompt only if not present

--- bashrc.d/prompt.sh
+++ bashrc.d/prompt.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Add a magic "red for failure" prompt state
-# This is escaped because at some point it gets unescaped in this sed
-
-# _red_dollar='\\[$([ $? -eq 0 ] || printf "\\e[1;31m")\\]\$\\[\\e[0m\\]'
-# export PS1="$(printf "%s" "$PS1" | sed 's/\\\$/'"$_red_dollar"'/')"
-# unset _red_dollar
+# Add a magic "red for failure" prompt state if not there already
+if [[ ! "$PS1" =~ 1\;31m\"\)\\]\\\$ ]]; then
+    # This is escaped because at some point it gets unescaped in this sed
+    # shellcheck disable=SC2016
+    _red_dollar='\\[$([ $? -eq 0 ] || printf "\\e[1;31m")\\]\$\\[\\e[0m\\]'
+    PS1="$(printf "%s" "$PS1" | sed 's/\\\$/'"$_red_dollar"'/')"
+    export PS1
+    unset _red_dollar
+fi
