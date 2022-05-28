3f520b9d4c7b0d19e5688aff148f4478384b262a
Nicholas Devenish <ndevenish@gmail.com>
2022-05-28 19:26:21 +0100

    Refactor install script to handle multiple shells

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ shopt -s dotglob
 
 # Make sure we weren't asked for usage
 if [[ "${1:-}" == -h || "${1:-}" == "--help" ]]; then
-    echo "Usage: ./install.sh"
+    echo "Usage: ./install.sh [-h|--help]"
     exit 0
 fi
 
@@ -28,8 +28,8 @@ done
 DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
 ########################################################################
 
+# Cross-platform functions - commands to elide platform differences
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    #alias readlink="readlink"
     if command -v "python3" 1>/dev/null 2>&1; then
         python=python3
     elif command -v "python" 1>/dev/null 2>&1; then
@@ -67,13 +67,16 @@ NC="$(printf "\033[0m")"
 # Keep track of if something failed
 FAIL=""
 
+########################################################################
+# Make softlinks in $HOME to everything in homedir/*
+
 echo -e "${BD}Installing/Updating existing .dotfiles links$NC\n"
 
 echo "Softlinks to homedir:"
 for item in "${DIR}"/homedir/*; do
     # If the target does not exist, link it
     link="$HOME/$(basename "$item")"
-    printf "    ~%-20s    " "${link#$HOME}"
+    printf "    ~%-20s    " "${link#"$HOME"}"
     if [[ ! -e "$link" && ! -d "$link" ]]; then
         ln -s "$item" "$link"
         echo "${G}New Link$NC"
@@ -105,6 +108,9 @@ for item in "${DIR}"/homedir/*; do
     fi
 done
 
+########################################################################
+# Make a .gitconfig-local for the main gitconfig to point to
+
 if [[ ! -f ~/.gitconfig-local ]]; then
     echo -e "\nCreating $B.gitconfig-local$NC"
     touch ~/.gitconfig-local
@@ -116,73 +122,104 @@ else
     echo -e "\n$B.gitconfig-local$NC already exists, not recreating"
 fi
 
+########################################################################
+# Updating shell init scripts to point to common utilities
 
+insert_or_replace_integration_in_file() {
+    # Usage: insert_or_replace_integration_in_file [-q] <filename> "<multi_line_text_to_replace>"
+    #
+    # The multi-line text should have a stable start and end line to delineate.
+    # Args:
+    #   -q  Do quietly - update without asking
+    _quiet=""
+    if [[ "$1" == "-q" ]]; then
+        _quiet=true
+        shift
+    fi
+    # Asks user to confirm if it's an update instead of a replacement
+    file_to_inject="$1"
+    lines_to_inject="$2"
+    start_line="$(echo "$lines_to_inject" | head -n 1)"
+    end_line="$(echo "$lines_to_inject" | tail -n 1)"
 
-echo -e "\nInjecting/Updating bash startup script"
+    if ! grep -sq "$start_line" "$file_to_inject"; then
+        echo "    No existing integration, writing to $B~${file_to_inject#"$HOME"}$NC"
+        # Easy case - there is no line in the file yet
+        echo "$lines_to_inject" >> "$file_to_inject"
+        echo "    Injected!"
+    else
+        echo "    Found existing integration in $B~${file_to_inject#"$HOME"}$NC"
+        _working_file=$(mktemp)
+        # We already have the lines in the file, and might be trying to update
+        sed -n '/'"$start_line"'/q;p' "$file_to_inject" > "$_working_file"
+        echo "$lines_to_inject" >> "$_working_file"
+        sed '1,/^'"$end_line"'/d' "$file_to_inject" >> "$_working_file"
 
-# All target files to consider for initialization
-_all_inits=(~/.bash_profile ~/.bashrc_local ~/.bashrc ~/.profile)
+        if ! cmp -s "$file_to_inject" "$_working_file"; then
+            echo "    There are changes to update:$W"
+            diff -u "$file_to_inject" "$_working_file" \
+            | tail -n +4 \
+            | sed -E \
+                -e 's/^(-.*)$/'"$R"'\1'"$W"'/' \
+                -e 's/^(\+.*)$/'"$G"'\1'"$W"'/' \
+                -e 's/^/        /' || true
+            _basename="$(basename "$file_to_inject")"
+            backup="$HOME/${_basename}.bak"
+            echo
+            read -p "$NC${BD}OK to proceed? [yN] $NC" -n 1 -r
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                echo "$NC   Copying existing file to $B$backup$NC"
+                ( set -x
+                    cp "$file_to_inject" "$backup"
+                    mv "$_working_file" "$file_to_inject"
+                )
+            else
+                echo "User declined to change. Skipping update."
+            fi
+        else
+            echo "    This file is up-to-date!"
+        fi
+    fi
+}
 
-# shellcheck disable=SC2016
-init_block='# >>> .dotfiles integration >>>
+if [[ "$SHELL" == */bash ]]; then
+    echo -e "\nThis users default shell is set to ${BD}bash$NC"
+
+    # All target files to consider for initialization
+    _all_inits=(~/.bash_profile ~/.bashrc_local ~/.bashrc ~/.profile)
+
+    # shellcheck disable=SC2016
+    init_block='# >>> .dotfiles integration >>>
 # !! Contents within this block are managed by .dotfiles installer !!
 for file in $(find "'"$DIR"'/bashrc.d" -name "*.sh" -o -name "*.bash" | sort -V); do
-  source "$file"
+    source "$file"
 done
 export PATH=$PATH:'"$DIR"'/bin
 # <<< .dotfiles integration <<<'
 
-
-# Try and find an existing file with this itegration block
-# (grep will exit with error even if it found some results)
-_init_file="$(grep -slm1 "# >>> .dotfiles integration >>>" "${_all_inits[@]}" 2>/dev/null)" || true
-if [[ -z "$_init_file" ]]; then
-    echo "    No existing integration"
-    # Find the first init file in our list that exists
-    for _init_file in "${_all_inits[@]}"; do
-        if [[ -f "$_init_file" ]]; then
-            break
-        fi
-    done
-    # Fail if we found nothing
+    start_line="$(echo "$init_block" | head -n 1)"
+    # Try and find an existing file with this integration block
+    # (grep will exit with error even if it found some results)
+    _init_file="$(grep -slm1 "$start_line" "${_all_inits[@]}" 2>/dev/null)" || true
     if [[ -z "$_init_file" ]]; then
-        echo "${R}Error: Could not find an init file to use from:"
-        echo "${_all_inits[@]}"
-        echo "$NC"
-    fi
-    echo "    Found $B~${_init_file#$HOME}$NC for injection"
-    echo "$init_block" >> "$_init_file"
-    echo "    Injected!"
-
-else
-    echo "    Found existing integration in $B~${_init_file#$HOME}$NC"
-    _working_file=$(mktemp)
-    sed -n '/# >>> .dotfiles integration >>>/q;p' "$_init_file" > "$_working_file"
-    echo "$init_block" >> "$_working_file"
-    sed '1,/^# <<< .dotfiles integration <<</d' "$_init_file" >> "$_working_file"
-
-    if ! cmp -s "$_init_file" "$_working_file"; then
-        echo "   There are changes to update:$W"
-        diff "$_init_file" "$_working_file" \
-        | sed -E \
-            -e 's/^(<.*)$/'"$R"'\1'"$W"'/' \
-            -e 's/^(>.*)$/'"$G"'\1'"$W"'/' \
-            -e 's/^/        /' || true
-        _basename="$(basename "$_init_file")"
-        backup="$HOME/.backup_${_basename#.}"
-        echo
-        read -p "$NC${BD}OK to proceed? [yN] $NC" -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            echo "$NC   Copying existing file to $B$backup$NC"
-            ( set -x
-                cp "$_init_file" "$backup"
-                mv "$_working_file" "$_init_file"
-            )
-        else
-            echo "User declined to change. Skipping update."
+        # Find the first init file in our list that exists
+        for _init_file in "${_all_inits[@]}"; do
+            if [[ -f "$_init_file" ]]; then
+                break
+            fi
+        done
+        # Fail if we found nothing
+        if [[ -z "$_init_file" ]]; then
+            echo "${R}Error: Could not find an init file to use from:"
+            echo "${_all_inits[@]}"
+            echo "$NC"
         fi
-    else
-        echo "    This file is up-to-date!"
     fi
+    if [[ -n "$_init_file" ]]; then
+        insert_or_replace_integration_in_file "$_init_file" "$init_block"
+    fi
+elif [[ "$SHELL" == */zsh ]]; then
+    echo -e "\nThis users default shell is set to ${BD}zsh$NC"
+
 fi
