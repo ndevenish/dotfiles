6f67e415490c2db7678dbc1b9f182dcb2a163ffa
Nicholas Devenish <ndevenish@gmail.com>
2022-05-29 22:01:06 +0100

    Set ls colors on gnu and bsd

--- zshrc.d/alias_and_settings.zsh
+++ zshrc.d/alias_and_settings.zsh
@@ -3,3 +3,15 @@ bindkey -e
 
 setopt autocd notify
 setopt EXTENDED_HISTORY
+
+# BSD-style, safe to always set
+export CLICOLOR=1
+# Detect GNU vs BSD ls to turn on colours
+if ls --color=auto; then
+    alias ls='ls --color=auto'
+    alias ll='ls --color=auto -lh'
+    alias lt='ls --color=auto -lrth'
+else
+    alias ll='ls -lh'
+    alias lt='ls -lrth'
+fi
