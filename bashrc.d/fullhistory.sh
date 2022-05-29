d7d52a68d5e83a4519aaa8c607518afd85776196
Nicholas Devenish <ndevenish@gmail.com>
2022-05-29 16:26:37 +0100

    Handle zsh differences in fullhistory

--- bashrc.d/fullhistory.sh
+++ bashrc.d/fullhistory.sh
@@ -3,9 +3,17 @@
 # Custom history file
 export CUSTOM_HISTORY_FILE=$HOME/.fullhistory
 gref() { grep "$@" ~/.fullhistory; }
-preexec_custom_history() {
-  echo "$HOSTNAME $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $*" >> "$CUSTOM_HISTORY_FILE"
-}
+
+if [[ -n ${ZSH_VERSION-} ]]; then
+  # ZSH doesn't split command over multiple variables
+  preexec_custom_history() {
+    echo "$HOSTNAME $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $1" >> "$CUSTOM_HISTORY_FILE"
+  }
+else
+  preexec_custom_history() {
+    echo "$HOSTNAME $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $*" >> "$CUSTOM_HISTORY_FILE"
+  }
+fi
 # Add it to the array of functions to be invoked each time.
 preexec_functions+=(preexec_custom_history)
 
---
+++ zshrc.d/fullhistory.zsh
@@ -0,0 +1,1 @@
+../bashrc.d/fullhistory.sh
