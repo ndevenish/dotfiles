17a79ee120a1828be66c2a3eb4fbf23d9c9abcb1
Nicholas Devenish <ndevenish@gmail.com>
2023-08-18 13:35:15 +0100

    Include pwd in history

--- bashrc.d/fullhistory.sh
+++ bashrc.d/fullhistory.sh
@@ -7,11 +7,11 @@ gref() { grep "$@" ~/.fullhistory; }
 if [[ -n ${ZSH_VERSION-} ]]; then
   # ZSH doesn't split command over multiple variables
   preexec_custom_history() {
-    echo "$HOSTNAME $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $1" >> "$CUSTOM_HISTORY_FILE"
+    echo "$HOSTNAME:\"$PWD\" $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $1" >> "$CUSTOM_HISTORY_FILE"
   }
 else
   preexec_custom_history() {
-    echo "$HOSTNAME $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $*" >> "$CUSTOM_HISTORY_FILE"
+    echo "$HOSTNAME:\"$PWD\" $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $*" >> "$CUSTOM_HISTORY_FILE"
   }
 fi
 # Add it to the array of functions to be invoked each time.
