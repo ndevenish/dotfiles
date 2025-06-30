11fa7d56100e901705aa7dc2a75a1d1189bd0a33
Nicholas Devenish <ndevenish@gmail.com>
2025-06-30 18:04:22 +0100

    History: Log time run in ms

--- bashrc.d/fullhistory.sh
+++ bashrc.d/fullhistory.sh
@@ -7,12 +7,12 @@ gref() { grep "$@" ~/.fullhistory; }
 if [[ -n ${ZSH_VERSION-} ]]; then
   # ZSH doesn't split command over multiple variables
   preexec_custom_history() {
-    _timer_hist_run="$SECONDS"
+    _timer_hist_run="$(date +%s.%N)"
     echo "$HOSTNAME:\"$PWD\" $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $1" >> "$CUSTOM_HISTORY_FILE"
   }
 else
   preexec_custom_history() {
-    _timer_hist_run="$SECONDS"
+    _timer_hist_run="$(date +%s.%N)"
     echo "$HOSTNAME:\"$PWD\" $$ $(date "+%Y-%m-%dT%H:%M:%S%z") $*" >> "$CUSTOM_HISTORY_FILE"
   }
 fi
@@ -22,10 +22,10 @@ precmd_custom_history() {
     local -i _exit=$?
     local -i _last_runtime=""
     if (( ${+_timer_hist_run} )); then
-        _last_runtime=$(($SECONDS-$_timer_hist_run))
+        _last_runtime=$(printf "%.0f" $((1000*($(date +%s.%N)-$_timer_hist_run))))
         unset _timer_hist_run
     fi
-    echo "##EXIT## $HOSTNAME pid=$$ \$?=$_exit t=$_last_runtime" >> "$CUSTOM_HISTORY_FILE"
+    echo "##EXIT## $HOSTNAME pid=$$ \$?=$_exit t_ms=$_last_runtime" >> "$CUSTOM_HISTORY_FILE"
 }
 # Add it to the array of functions to be invoked each time.
 preexec_functions+=(preexec_custom_history)
